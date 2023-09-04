891ad84cf23159d054791ec33fc205e9539ca281
alexcom <a.kravets84@gmail.com>
2023-09-04 15:14:19 +0300

    Update script

--- build_deploy.sh
+++ build_deploy.sh
@@ -3,7 +3,7 @@
 rm -rf ./target || echo no target directory found
 rm podcast_backup_lambda.zip || echo no deploy zip found
 mkdir ./target
-GOOS=linux go build -o ./target/podcast_backup podcast_backup.go
+GOOS=linux go build -o ./target/podcast_backup
 
 # as advised in https://aws.amazon.com/premiumsupport/knowledge-center/lambda-deployment-package-errors/
 chmod 644 "$(find ./target -type f)"
